8a780f94b339cedd2fc313f106b07d71aa332875
Magdalena Lesisz <meghara@icloud.com>
2023-08-22 20:19:57 +0200

    Dokończenie funkcji titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log(event);  
+  event.preventDefault();  
+  const clickedElement = this;  
   console.log('Link was clicked!');
+  
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,9 +14,12 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.post');
 
@@ -24,9 +29,17 @@ for(let activeLink of activeLinks){
   
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector:', articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle:', targetArticle);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
